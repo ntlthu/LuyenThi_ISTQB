2c46ddf702566cac6e52c08dfedf6cd421b13847
Thu <thuntl.vnit@gmail.com>
2026-01-25 18:10:05 +0700

    2th

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-  function toggleVi() {
-    document.querySelectorAll('.vi').forEach(el => {
-      el.classList.toggle('hidden');
-    });
-  }
+function toggleVi(el) {
+  const group = el.closest('.question-group');
 
-function toggleAnswer() {
-  document.querySelectorAll('.answer')
-    .forEach(el => el.classList.toggle('show'));
-
-  document.querySelector('.fa-lightbulb')
-    .classList.toggle('fa-circle-check');
+  group.querySelectorAll('.vi').forEach(v => {
+    v.classList.toggle('hidden');
+  });
 }
+
+
+
+function toggleAnswer(el) {
+  const group = el.closest('.question-group');
+  const answer = group.querySelector('.answer');
+
+  answer.classList.toggle('show');
+
+  el.classList.toggle('fa-circle-check');
+  el.classList.toggle('fa-lightbulb');
+}
+
 
